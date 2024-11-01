88c5fd35e41692fddfcddc88db422bc26e431a0a
leneribeiro <rosileneolegario24@gmail.com>
2024-11-01 16:53:40 -0300

    Add files via upload
    pesquise pelos filmes divertida mente 2, barbie, wonderland, o auto da compadecida 2, é assim que acaba, para testa o código.

---
+++ dados.js
@@ -0,0 +1,35 @@
+let dados = [
+    {
+        titulo: "Divertida mente 2",
+        descricao: "ano de lançamento: 2024 ",
+        link: "https://pt.wikipedia.org/wiki/Inside_Out_2",
+        tags: "divertidamente Divertidamante2 DIVERTIDAMENTE 2 DIVERTIDAMANTE2 divertida mente "
+    },
+    {
+        titulo: "Barbie",
+        descricao: "ano de lançamento: 2024 ",
+        link: "https://pt.wikipedia.org/wiki/Barbie_(filme)",
+        tags: " barbie BARBIE Barbie rosa ROSA Rosa "
+    },
+    {
+        titulo: "Wonderland",
+        descricao: "ano de lançamento: 2024",
+        link: "https://asiaon.com.br/conheca-wonderland-ficcao-cientifica-com-suzy-e-park-bo-gum-que-chega-em-breve-na-netflix/",
+        tags: " WONDERLAND wonderland  suzi Suzi SUZI PARK BO GUM park bo gum "
+    },
+
+
+    {
+        titulo: "O auto da compadecida 2",
+        descricao: "ano que vai ser lançado: 2024 ",
+        link: "https://gshow.globo.com/cultura-pop/filmes/noticia/o-auto-da-compadecida-2-ganha-data-de-estreia-para-dezembro-de-2024.ghtml",
+        tags: " oautodacompadecida O AUTO DA COMPADECIDA o auto da compadecida2 2 joão grilo JOÃO GRILO chico  CHICO "
+    }, 
+
+    {
+        titulo: "É assim que acaba ",
+        descricao: "ano de lançamento: 2024 ",
+        link: "https://cinepop.com.br/e-assim-que-acaba-433794/",
+        tags: " É ASSIM QUE ACABA E ASSIM QUE ACABA e assim que acaba "
+    }
+];
